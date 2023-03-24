84d13739324347fd64459568a6b8f8411b9d984a
jiwonkim-tsp <126739419+jiwonkim-tsp@users.noreply.github.com>
2023-03-24 16:55:52 +0900

    Feat: solutionData 수정

--- src/data/solutionData.ts
+++ src/data/solutionData.ts
@@ -17,12 +17,10 @@ export const solutionPageData: ISolutionData[] = [
     title: "웹 기반 XR 디지털트윈 시스템",
     sentence: [
       {
-        id: "1-1",
         content: "웹 기반 디지털트윈",
         color: "green",
       },
       {
-        id: "1-2",
         content:
           "은 빠른 현장적용이 가능하고 AR/VR 기술을 활용하여 효율적으로 시설물을 관리할 수 있습니다",
         color: "white",
@@ -35,17 +33,14 @@ export const solutionPageData: ISolutionData[] = [
     title: "웹 탑재 딥러닝을 이용한 AR 광고",
     sentence: [
       {
-        id: "2-1",
         content: "AR과 딥러닝 기술을 이용한 간편하고 효과적인 ",
         color: "white",
       },
       {
-        id: "2-2",
         content: "웹 기반 광고 서비스",
         color: "green",
       },
       {
-        id: "2-3",
         content: "를 경험해보세요",
         color: "white",
       },
@@ -57,17 +52,14 @@ export const solutionPageData: ISolutionData[] = [
     title: "공간인식 기술을 활용한 AR 도슨트",
     sentence: [
       {
-        id: "3-1",
         content: "공간인식 기술을 활용한",
         color: "white",
       },
       {
-        id: "3-2",
         content: " AR 도슨트 서비스",
         color: "yellow",
       },
       {
-        id: "3-3",
         content: "를 통해 과거 유적지를 생생하게 재현할 수 있습니다",
         color: "white",
       },
