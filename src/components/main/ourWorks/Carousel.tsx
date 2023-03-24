cb843bf4979165441dc80df1ab978a5003cd79ba
jiwonkim-tsp <126739419+jiwonkim-tsp@users.noreply.github.com>
2023-03-24 18:11:56 +0900

    Feat: our work 슬라이더 수정

--- src/components/main/ourWorks/Carousel.tsx
+++ src/components/main/ourWorks/Carousel.tsx
@@ -1,25 +1,11 @@
 import { $Wrapper, $CarouselWrapper, $Item } from "./styleCarousel";
-import { ourWorkData } from "@Data/ourWorkData";
-//slick css
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { carouselData } from "@Data/ourWorkData";
 
 const Carousel = () => {
-  const settings = {
-    arrows: false,
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 9000,
-    autoplaySpeed: 0,
-    cssEase: "linear",
-  };
   return (
     <$Wrapper>
-      <$CarouselWrapper {...settings}>
-        {ourWorkData.map((list) => (
+      <$CarouselWrapper>
+        {carouselData.map((list) => (
           <$Item key={list.id}>
             <img src={list.img} alt={list.title} />
           </$Item>
--- src/components/main/ourWorks/styleCarousel.tsx
+++ src/components/main/ourWorks/styleCarousel.tsx
@@ -1,20 +1,29 @@
-import styled from "styled-components";
-import Slider from "react-slick";
+import styled, { keyframes } from "styled-components";
+import { motion } from "framer-motion";
+
+const infinite = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(calc(-35vw * 12));
+  }
+`;
 
 export const $Wrapper = styled.div`
   position: relative;
   width: 100%;
 `;
 
-export const $CarouselWrapper = styled(Slider)`
-  transition-timing-function: linear;
-  -webkit-transition-timing-function: linear !important;
-  -o-transition-timing-function: linear !important;
-  transition-timing-function: linear !important;
+export const $CarouselWrapper = styled(motion.ul)`
+  width: calc(35vw * 24);
+  display: flex;
+  animation: ${infinite} 40s linear infinite;
 `;
 
-export const $Item = styled.div`
+export const $Item = styled(motion.li)`
+  width: 35vw;
   img {
-    width: 30vw;
+    width: 32vw;
   }
 `;
